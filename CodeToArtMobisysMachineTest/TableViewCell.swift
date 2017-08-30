66c38f9e523f863e1e077620fb968f93c763f5ba
harshad kekane <harshadkekane@harshads-imac.local>
2017-08-30 13:21:56 +0530

    Initial Commit

---
+++ CodeToArtMobisysMachineTest/TableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  TableViewCell.swift
+//  CodeToArtMobisysMachineTest
+//
+//  Created by harshad kekane on 30/08/17.
+//  Copyright © 2017 harshad kekane. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet var imageViewCell: UIImageView!
+    
+    @IBOutlet var dateLblCell: UILabel!
+    
+    @IBOutlet var nameLabelCell: UILabel!
+    
+    @IBOutlet var categoryLblCell: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Pods/Target Support Files/Pods-CodeToArtMobisysMachineTest/Pods-CodeToArtMobisysMachineTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CodeToArtMobisysMachineTest : NSObject
+@end
+@implementation PodsDummy_Pods_CodeToArtMobisysMachineTest
+@end
